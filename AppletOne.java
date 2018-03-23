a0cc91482a6c3252df7cb18c7421fcbc4d9ef03d
s6csb <s6csb@ccf.rajagiricentral>
2018-03-23 12:15:37 +0530

    Applet

---
+++ AppletOne.java
@@ -0,0 +1,13 @@
+//Java Program for simple appelet
+
+import java.awt.*;
+import java.applet.*;
+/*
+<applet code="AppletOne" width=350 height=350> </applet>
+*/
+public class AppletOne extends Applet {
+public void paint(Graphics g) {
+g.setColor(Color.black);
+g.drawString("Hello World", 20, 20);
+}
+}
